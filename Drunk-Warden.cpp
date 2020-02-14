c41cd15be47f49a940ced2e55961037b00e42dca
Viet Thien Tran <47939979+Greenhat1998@users.noreply.github.com>
2020-02-14 19:12:53 +0700

    Sample code for End-year Party Contest

---
+++ Drunk-Warden.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int n, t;
+int main()
+{
+    cin>>t;
+    while (t--)
+    {
+        cin>>n;
+        cout<<(int)(sqrt(n))<<'\n';
+    }
+    return 0;
+}
