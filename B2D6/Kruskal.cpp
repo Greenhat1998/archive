ee226d585f8ff8097942deafb734825dc549c755
Viet Thien Tran <47939979+Greenhat1998@users.noreply.github.com>
2020-06-10 13:56:07 +0700

    Update Kruskal.cpp

--- B2D6/Kruskal.cpp
+++ B2D6/Kruskal.cpp
@@ -12,8 +12,8 @@ int cmp(edge &X, edge &Y)
 }
 int root(int p) 
 {
-    if (T[p]==p)	return p;
-    else return T[p]=root(T[p]);
+    	if (T[p]==p)	return p;
+    	else return T[p]=root(T[p]);
 }
 void join(int p, int q)
 { 
@@ -21,17 +21,17 @@ void join(int p, int q)
 }
 int main() 
 {
-    cin>>n>>m;
-    for(int i=1;i<=n;i++)	T[i]=i;
-    for(int i=1;i<=m;i++)	cin>>a[i].u>>a[i].v>>a[i].w;
-    sort(a+1,a+m+1,cmp);
-    for(int i=1;i<=m;i++) 
-        if (root(a[i].u) != root(a[i].v)) 
+    	cin>>n>>m;
+    	for(int i=1;i<=n;i++)	T[i]=i;
+    	for(int i=1;i<=m;i++)	cin>>a[i].u>>a[i].v>>a[i].w;
+    	sort(a+1,a+m+1,cmp);
+    	for(int i=1;i<=m;i++) 
+        	if (root(a[i].u) != root(a[i].v)) 
 		{
-            join(a[i].u,a[i].v);
-            cout<<a[i].u<<' '<<a[i].v<<'\n';
-            res=res+a[i].w;
-        }
-    cout<<res;
-    return 0;
+            		join(a[i].u,a[i].v);
+            		cout<<a[i].u<<' '<<a[i].v<<'\n';
+            		res=res+a[i].w;
+        	}
+    	cout<<res;
+    	return 0;
 }
