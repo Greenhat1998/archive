7e99114c5ee76df016c56a2c46eb1b9f1e76b0e4
Viet Thien Tran <47939979+Greenhat1998@users.noreply.github.com>
2020-06-12 10:29:11 +0700

    Update Convert.cpp

--- B2D6/Convert.cpp
+++ B2D6/Convert.cpp
@@ -7,7 +7,7 @@ int a[1000][1000];		// a[][] ma tran ke
 canh e[1000];			// e[] danh sach canh
 vector<int> b[1000];	// vector v[] danh sach ke
 int n, m;
-void mtk_dsc(int a[][], canh e[])	// chuyen ma tran ke thanh danh sach canh
+void mtk_dsc(int a[1000][1000], canh e[1000])	// chuyen ma tran ke thanh danh sach canh
 {
 	m=0;
 	for(int i=1;i<=n;i++)
@@ -18,7 +18,7 @@ void mtk_dsc(int a[][], canh e[])	// chuyen ma tran ke thanh danh sach canh
 				e[m].x=i, e[m].y=j;
 			}
 }
-void dsc_mtk(canh e[], int a[][])	// chuyen danh sach canh thanh ma tran ke
+void dsc_mtk(canh e[1000], int a[1000][1000])	// chuyen danh sach canh thanh ma tran ke
 {
 	for(int i=1;i<=m;i++)	
 	{
@@ -26,27 +26,27 @@ void dsc_mtk(canh e[], int a[][])	// chuyen danh sach canh thanh ma tran ke
 		a[u][v]=a[v][u]=1;
 	}
 }
-void mtk_dsk(int a[][], vector<int> b[])	// ma tran ke thanh danh sach ke
+void mtk_dsk(int a[1000][1000], vector<int> b[1000])	// ma tran ke thanh danh sach ke
 {
 	for(int i=1;i<=n;i++)
 		for(int j=1;j<=n;j++)	
 			if (a[i][j]==1)	b[i].push_back(j);
 }
-void dsk_mtk(vector<int> b[], int a[][])	// danh sach ke thanh ma tran ke
+void dsk_mtk(vector<int> b[1000], int a[1000][1000])	// danh sach ke thanh ma tran ke
 {
 	for(int i=1;i<=n;i++)
 		for(int j=0;j<b[i].size();j++)	a[i][b[i][j]]=1;
 }
-void dsc_dsk(canh e[], vector<int> b[])		// danh sach canh thanh danh sach ke
+void dsc_dsk(canh e[1000], vector<int> b[1000])		// danh sach canh thanh danh sach ke
 {
 	for(int i=1;i<=m;i++)
 	{
 		int u=e[i].x, v=e[i].y;
-		b[x].push_back(y);
-		b[y].push_back(x);
+		b[u].push_back(v);
+		b[v].push_back(u);
 	}
 }
-void dsk_dsc(vector<int> b, canh e[])		// danh sach ke thanh danh sach canh
+void dsk_dsc(vector<int> b[1000], canh e[1000])		// danh sach ke thanh danh sach canh
 {
 	canh tmp[1000];
 	int cnt=0;
