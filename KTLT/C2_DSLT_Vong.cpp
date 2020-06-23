c50e11e319feedb8acae1939346024c9420f77e0
Viet Thien Tran <47939979+Greenhat1998@users.noreply.github.com>
2020-06-23 16:21:38 +0700

    Update C2_DSLT_Vong.cpp

--- KTLT/C2_DSLT_Vong.cpp
+++ KTLT/C2_DSLT_Vong.cpp
@@ -31,16 +31,16 @@ void insert_first(node &tail, int X)
 void insert_last(node &tail, int X) 
 {
     insert_first(tail,X);
-	tail=tail->next;
+    tail=tail->next;
 }
 void insert(node &tail, int X, int pos) // danh so tu 1...
 {
     if (pos<1 || pos>length+1) cout<<"Vi tri loi!\n";
     else if (tail==NULL || pos==1)	insert_first(tail,X);
-	else{
-		node tmp=make(X);
-		node q=tail;
-		for(int i=1;i!=pos;i++)	q=q->next;
+    else{
+	node tmp=make(X);
+	node q=tail;
+	for(int i=1;i!=pos;i++)	q=q->next;
         tmp->next = q->next;
         q->next=tmp;
         if (pos==length+1) tail=tmp;
@@ -48,13 +48,13 @@ void insert(node &tail, int X, int pos) // danh so tu 1...
 }
 node del(node &tail, int pos)
 {
-	node prev=tail;
+    node prev=tail;
     if (pos<1 || pos>length) cout<<"Vi tri loi!\n";
     else if (length==1)	tail=NULL;
-	else {
+    else {
         node tmp=tail->next;
         for(int i=1;i<pos;i++) 
-		{
+	{
             prev=tmp;
             tmp=tmp->next;
         }
@@ -66,27 +66,27 @@ node del(node &tail, int pos)
 }
 void input(node &tail)
 {
-	int n, X;
-	cin>>n;
-	for(int i=1;i<=n;i++)
-	{
-		cin>>X;
-		insert_last(tail,X);
-	}
+    int n, X;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+	cin>>X;
+	insert_last(tail,X);
+    }
 }
 void output(node tail) 
 {
     node tmp=tail;
     if (tail!=NULL)
     	do {
-        	tmp=tmp->next;
-        	cout<<tmp->data<<' ';
+            tmp=tmp->next;
+            cout<<tmp->data<<' ';
     	} while (tmp!=tail);
 }
 int main()
 {
-	input(tail);
-	output(tail);
-	cout<<'\n'<<length;
+    input(tail);
+    output(tail);
+    cout<<'\n'<<length;
     return 0;
 }
